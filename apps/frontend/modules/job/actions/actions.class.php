a60330ecd1427e275a2f01de741e425c976de29a
Axo <axelforest@gmail.com>
2011-11-03 12:46:29 -0400

    Day 5 - The Routing

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -19,8 +19,11 @@ class jobActions extends sfActions
 
   public function executeShow(sfWebRequest $request)
   {
-    $this->job = Doctrine_Core::getTable('jobeetJob')->find(array($request->getParameter('id')));
-    $this->forward404Unless($this->job);
+//    $this->job = Doctrine_Core::getTable('jobeetJob')->find(array($request->getParameter('id')));
+//    $this->forward404Unless($this->job);
+    $this->job = $this->getRoute()->getObject();
+ 
+//    $this->forward404Unless($this->job);
   }
 
   public function executeNew(sfWebRequest $request)
