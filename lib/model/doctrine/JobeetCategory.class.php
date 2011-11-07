89abb1155a7531f2e006be8c7d7dfea91330dfd7
Axo <axelforest@gmail.com>
2011-11-07 17:54:26 -0500

    Day 7 - Playing with the Category Page

--- lib/model/doctrine/JobeetCategory.class.php
+++ lib/model/doctrine/JobeetCategory.class.php
@@ -11,15 +11,27 @@
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
 class JobeetCategory extends BaseJobeetCategory
-{
+{    
     // lib/model/doctrine/JobeetCategory.class.php
+    public function getActiveJobsQuery()
+    {
+        $q = Doctrine_Query::create()
+        ->from('JobeetJob j')
+        ->where('j.category_id = ?', $this->getId());
+
+        return Doctrine_Core::getTable('JobeetJob')->addActiveJobsQuery($q);
+    }
+    
     public function getActiveJobs($max = 10)
     {
-      $q = Doctrine_Query::create()
-        ->from('JobeetJob j')
-        ->where('j.category_id = ?', $this->getId())
+        $q = $this->getActiveJobsQuery()
         ->limit($max);
 
-      return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
+        return $q->execute();
+    }
+
+    public function countActiveJobs()
+    {
+        return $this->getActiveJobsQuery()->count();
     }
 }
