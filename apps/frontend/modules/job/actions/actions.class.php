2fd1b431e8f30db36eb5e4701c48c0a1263b6d03
Axo <axelforest@gmail.com>
2011-12-08 17:08:31 -0500

    Day 11 - Testing your Forms

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -61,7 +61,10 @@ class jobActions extends sfActions
     
     public function executeEdit(sfWebRequest $request)
     {
-      $this->form = new JobeetJobForm($this->getRoute()->getObject());
+      $job = $this->getRoute()->getObject();
+      $this->forward404If($job->getIsActivated());
+
+      $this->form = new JobeetJobForm($job);
     }
 
     public function executeUpdate(sfWebRequest $request)
@@ -95,4 +98,17 @@ class jobActions extends sfActions
         $this->redirect('job_show', $job);
       }
     }
+    
+    public function executeExtend(sfWebRequest $request)
+    {
+      $request->checkCSRFProtection();
+
+      $job = $this->getRoute()->getObject();
+      $this->forward404Unless($job->extend());
+
+      $this->getUser()->setFlash('notice', sprintf('Your job validity has been extended until %s.', $job->getDateTimeObject('expires_at')->format('m/d/Y')));
+
+      $this->redirect('job_show_user', $job);
+    }
+
 }
